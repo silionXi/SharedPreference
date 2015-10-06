64b23c6f38c65485461e2a0a31ddee8cfdfc1513
silion <lslshadow@163.com>
2015-10-06 23:51:15 +0800

    数据存储之SharedPreference

---
+++ SharedPreferenceDemo/app/src/main/java/com/silion/sharedpreferencedemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.silion.sharedpreferencedemo;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mNameEditText;
+    EditText mPassWordEditText;
+    CheckBox mSaveCheckBox;
+    Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNameEditText = (EditText) findViewById(R.id.editUserName);
+        mPassWordEditText = (EditText) findViewById(R.id.editUserPassWord);
+        mSaveCheckBox = (CheckBox) findViewById(R.id.saveCheckBox);
+        SharedPreferences sharedPre = getSharedPreferences("my_sharedPre", MODE_PRIVATE);
+        editor = sharedPre.edit();
+        String name = sharedPre.getString("name", "");
+        mNameEditText.setText(name);
+        Boolean isCheck = sharedPre.getBoolean("save", false);
+        mSaveCheckBox.setChecked(isCheck);
+    }
+
+    public void doClick(View view) {
+        switch (view.getId()) {
+            case R.id.login: {
+                String name = mNameEditText.getText().toString();
+                String passWord = mPassWordEditText.getText().toString().trim();
+                if (name.equals("xixi") && passWord.equals("123456")) {
+                    if (mSaveCheckBox.isChecked()) {
+                        editor.putString("name", name);
+                        editor.putBoolean("save", true);
+                        editor.commit();
+                    } else {
+                        editor.remove("name");
+                        editor.remove("save");
+                        editor.commit();
+                    }
+                    Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "账号或密码失败", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+            case R.id.cancel: {
+                finish();
+            }
+            default:
+                break;
+        }
+    }
+}
